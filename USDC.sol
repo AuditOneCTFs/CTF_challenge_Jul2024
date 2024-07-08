837b0ebcfd57433567af0fc3d1fd93ecd16ea8c3
Gracious Igwe <graciousigwe@Gracious-MacBook-Pro.local>
2024-07-08 15:51:20 +0100

    chore: forge init

---
+++ USDC.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.24;
+
+import 'lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol';
+
+contract USDC is ERC20 {
+
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
+
+    function decimals() public pure override returns (uint8) {
+        return 6;
+    }
+
+    function mint(address account, uint256 value) external {
+        _mint(account, value);
+    }
+}
